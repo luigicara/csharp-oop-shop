0d8623f0eea07d9f0daf283b6676bb95997d1781
luigicara <luigi.caravelli98@gmail.com>
2023-04-07 16:20:44 +0200

    Add project files.

---
+++ csharp-oop-shop/Product.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_shop
+{
+    internal class Product
+    {
+        public int Code{ get; private set; }
+        public string? Name{ get; set; }
+        public string? Description{ get; set; }
+        public decimal Price{ get; set; }
+        public int Vat{ get; set; }
+
+        public Product(string name = "", string description = "", decimal price = 0, int vat = 22)
+        {
+            Code = new Random().Next(1, 999999);
+            Name = name;
+            Description = description;
+            Price = price;
+            Vat = vat;
+        }
+
+        public string GetCodeWithPadLeft()
+        {
+            return Code.ToString().PadLeft(8, '0');
+        }
+
+        public decimal PriceVatIncluded()
+        {
+            return Decimal.Round(((Price / 100) * Vat) + Price, 2);
+        }
+
+        public string ExtendedName()
+        {
+            return GetCodeWithPadLeft() + " " + Name;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Codice e Nome: {ExtendedName()}");
+            Console.WriteLine($"Descrizione: {Description}");
+            Console.WriteLine($"Prezzo con iva: {PriceVatIncluded()}");
+            Console.WriteLine($"Prezzo senza iva: {Price}");
+            Console.WriteLine($"Iva: {Vat}%\r\n");
+        }
+    }
+}
---
+++ csharp-oop-shop/Program.cs
@@ -0,0 +1,21 @@
+﻿using csharp_oop_shop;
+
+internal class Program
+{
+    private static void Main()
+    {
+        Product[] products = {
+         new Product("Penne al sugo", "Buonissime penne al sugo", 12.00m),
+         new Product("Linguine ai frutti di mare", "Buonissime linguine ai frutti di mare", 18.00m),
+         new Product("Spaghetti alla bolognese", "Buonissimi spaghetti alla bolognese", 17.00m),
+        };
+
+        Console.WriteLine("Elenco Prodotti\r\n");
+
+        for (int i = 0; i < products.Length; i++)
+        {
+            products[i].PrintInfo();
+        }
+
+    }
+}
